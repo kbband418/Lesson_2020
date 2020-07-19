79c88e066e01882546249b12a55266939da5cec3
AlvinLiu <kb_band418@hotmail.com>
2020-07-19 20:35:20 +0800

    Upload Lesson1 and 2_A

---
+++ New Unity Project/Assets/Lesson_1/Lesson_1.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class Lesson_1 : MonoBehaviour
+{
+    public void Awake()
+    {
+        Application.targetFrameRate = 30;
+    }
+
+    public Vector3 m_Point_1 = Vector3.zero;
+    public Vector3 m_Point_2 = Vector3.zero;
+    public Vector3 m_Point_3 = Vector3.zero;
+
+    public Vector3 m_MouseVector = Vector3.zero;
+    public Vector3 m_TranMouseVector = Vector3.zero;
+
+    public bool m_isIntTriangle = false;
+
+    private void OnGUI()
+    {
+        Handles.DrawLine(m_Point_1, m_Point_2);
+        Handles.DrawLine(m_Point_2, m_Point_3);
+        Handles.DrawLine(m_Point_3, m_Point_1);
+
+        m_MouseVector = Input.mousePosition;
+        m_TranMouseVector = m_MouseVector;
+        m_TranMouseVector.y = 720f - m_MouseVector.y;
+
+        m_isIntTriangle = InThisTriangle(m_TranMouseVector, m_Point_1, m_Point_2, m_Point_3);
+
+        if (m_isIntTriangle)
+        {
+            Handles.color = Color.green;
+        }
+        else
+        {
+            Handles.color = Color.white;
+        }
+    }
+
+    bool InThisTriangle(Vector3 _MousePoint, Vector3 _Point1, Vector3 _Point2, Vector3 _Point3)
+    {
+        float UHat = 0f;
+        float VHat = 0f;
+
+        VHat = (_MousePoint.x * (_Point2.y - _Point1.y) - _Point1.x * (_Point2.y - _Point1.y) - _MousePoint.y * (_Point2.x - _Point1.x) + _Point1.y * (_Point2.x - _Point1.x)) / ((_Point3.x - _Point1.x) * (_Point2.y - _Point1.y) - (_Point3.y - _Point1.y) * (_Point2.x - _Point1.x));
+        UHat = (_MousePoint.y - _Point1.y - VHat * (_Point3.y - _Point1.y)) / (_Point2.y - _Point1.y);
+
+
+        if (UHat >= 0f && VHat >= 0f && (UHat + VHat) <= 1f)
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
---
+++ New Unity Project/Assets/Lesson_2/Lesson_2_A.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class Lesson_2_A : MonoBehaviour
+{
+    public void Awake()
+    {
+        Application.targetFrameRate = 30;
+    }
+
+    public Vector3 m_Point_1 = Vector3.zero;
+    public Vector3 m_Point_2 = Vector3.zero;
+
+    public Vector3 m_MoveVector = Vector2.zero;
+
+    public Vector3 m_NewPoint_1 = Vector3.zero;
+    public Vector3 m_NewPoint_2 = Vector3.zero;
+
+    public int m_MaxDegree = 0;
+    public int m_Degree = 0;
+
+
+    private void OnGUI()
+    {
+        m_Degree++;
+        if (m_Degree == m_MaxDegree)
+            m_Degree = 0;
+        m_NewPoint_1.x = m_MoveVector.x + Mathf.Cos(GetRadian(m_Degree)) * m_Point_1.x + (-1) * Mathf.Sin(GetRadian(m_Degree)) * m_Point_1.y;
+        m_NewPoint_1.y = (1) * (m_MoveVector.y) + Mathf.Sin(GetRadian(m_Degree)) * m_Point_1.x + Mathf.Cos(GetRadian(m_Degree)) * m_Point_1.y;
+        m_NewPoint_2.x = m_MoveVector.x + Mathf.Cos(GetRadian(m_Degree)) * m_Point_2.x + (-1) * Mathf.Sin(GetRadian(m_Degree)) * m_Point_2.y;
+        m_NewPoint_2.y = (1) * (m_MoveVector.y) + Mathf.Sin(GetRadian(m_Degree)) * m_Point_2.x + Mathf.Cos(GetRadian(m_Degree)) * m_Point_2.y;
+     
+        Handles.DrawLine(m_NewPoint_1, m_NewPoint_2);
+    }
+
+    float GetRadian(int degreed)
+    {
+        return degreed * Mathf.PI / 180;
+    }
+}
