0e4b9c030df98981973d49f7db120f8be4e4c167
AlvinLiu <kb_band418@hotmail.com>
2020-09-14 23:58:28 +0800

    Lesson_5 雷達(完成)

--- New Unity Project/Assets/Lesson_5/Lesson_5.cs
+++ New Unity Project/Assets/Lesson_5/Lesson_5.cs
@@ -8,30 +8,36 @@ public class Lesson_5 : MonoBehaviour
         Application.targetFrameRate = 30;
     }
 
+    [Header("扇形圓心座標")]
     public Vector3 m_BasePoint_1 = new Vector3(0, 0, 0);
 
-
-    public Vector3 m_MouseVector = Vector3.zero;
-    public Vector3 m_TranMousePoint = Vector3.zero;
-
-
-
-
+    //滑鼠在螢幕上的座標
+    private Vector3 m_MouseVector = Vector3.zero;
+    //滑鼠在螢幕上的座標轉成直角座標系
+    private Vector3 m_TranMousePoint = Vector3.zero;
 
     [Header("扇形長度")]
     public float m_fLineLength = 100f;
     [Header("扇形角度")]
     [Range(0, 360)]
     public int m_iCircleAngle = 1;
+    [Header("扇形畫線密度")]
+    [Range(1, 10)]
+    public int m_iLineDensity = 1;
     [Header("旋轉速度")]
     public int m_iRotateSpeed = 0;
+    [Header("是否在扇形角度內")]
+    public bool m_bIsInSector = false;
+
 
     private Vector3 m_ScreenBaseVector = new Vector3(0, 0, 0);
     private Matrix4x4 m_RotationMatrix = new Matrix4x4();
     private int m_iAddRotateDegree = 0;
     private float m_fDegree = 0;
-    private int m_iLineDensity = 3;
 
+    //紀錄新的基準點1 2的座標
+    private Vector3 NewBasePoint_1 = Vector3.zero;
+    private Vector3 NewBasePoint_2 = Vector3.zero;
 
     void OnGUI()
     {
@@ -42,8 +48,8 @@ public class Lesson_5 : MonoBehaviour
         //以目標扇形角度算出左右起始及終點角度，表示扇形張開範圍
         int TempDegree = (m_iCircleAngle % 2 != 0) ? (m_iCircleAngle - 1) : m_iCircleAngle;
 
-        //概念以很多條線組成，畫出扇形，依照密度來算出需要使用多少條線來畫扇形
-        int LineCount = m_iCircleAngle * m_iLineDensity;
+        //概念: 以很多條線組成，畫出扇形，依照密度來算出需要使用多少條線來畫扇形
+        int LineCount = TempDegree * m_iLineDensity;
 
         //每條線間隔的角度是多少
         float DegreePerLine = ((float)TempDegree) / LineCount;
@@ -54,13 +60,17 @@ public class Lesson_5 : MonoBehaviour
         BasePoint_2.y = m_BasePoint_1.y + m_fLineLength;
         BasePoint_2.z = m_BasePoint_1.z;
 
-        //目前旋轉幾度
-        m_iAddRotateDegree += m_iRotateSpeed;
-        if (m_iAddRotateDegree >= 360 || m_iAddRotateDegree <= -360) m_iAddRotateDegree = 0;
+        //目前旋轉幾度，如果再扇形內就停止旋轉
+        if (m_bIsInSector == false)
+            m_iAddRotateDegree += m_iRotateSpeed;
+        if (m_iAddRotateDegree >= 360)
+            m_iAddRotateDegree -= 360;
+        else if(m_iAddRotateDegree <= -360)
+            m_iAddRotateDegree += 360;
 
-        //起始角度
-        int iStartDegree = (-1) * (TempDegree / 2);
-        
+        //扇形左右邊各張開的角度
+        int iSideDegree = (TempDegree / 2);
+
         //基準點1、2轉成原點的暫存
         Vector3 TempBasePoint_1 = Vector3.zero;
         Vector3 TempBasePoint_2 = Vector3.zero;
@@ -69,18 +79,19 @@ public class Lesson_5 : MonoBehaviour
         Vector3 NewPoint_1 = Vector3.zero;
         Vector3 NewPoint_2 = Vector3.zero;
 
-        //扇形起始向量
-        Vector3 StartVector = Vector3.zero;
-        //扇形結尾向量
-        Vector3 EndVector = Vector3.zero;
+        //基準點的座標位在線集合的哪個Index
+        int BasePointIndex = LineCount / 2;
 
-
-        for (int index = 0; index <= LineCount; index ++)
+        for (int index = 0; index < LineCount + 1; index++)
         {
             m_RotationMatrix = Matrix4x4.identity;
 
             //這條線要旋轉幾度
-            m_fDegree = iStartDegree + DegreePerLine * index + m_iAddRotateDegree;
+            m_fDegree = (-1) * iSideDegree + DegreePerLine * index + m_iAddRotateDegree;
+            if (m_fDegree >= 360)
+                m_fDegree -= 360;
+            else if (m_fDegree <= -360)
+                m_fDegree += 360;
 
             m_RotationMatrix.m00 = Cos(m_fDegree);
             m_RotationMatrix.m01 = (-1) * Sin(m_fDegree);
@@ -97,37 +108,62 @@ public class Lesson_5 : MonoBehaviour
             TempBasePoint_2 = new Vector3(BasePoint_2.x - m_BasePoint_1.x, (-1) * (BasePoint_2.y - m_BasePoint_1.y), BasePoint_2.z - m_BasePoint_1.z);
             NewPoint_1 = m_RotationMatrix.MultiplyPoint(TempBasePoint_1);
             NewPoint_2 = m_RotationMatrix.MultiplyPoint(TempBasePoint_2);
+            if(index == BasePointIndex)
+            {
+                NewBasePoint_1 = NewPoint_1 - m_ScreenBaseVector;
+                NewBasePoint_1.y *= (-1);
+                NewBasePoint_2 = NewPoint_2 - m_ScreenBaseVector;
+                NewBasePoint_2.y *= (-1);
+            }
             Handles.color = Color.green;
             Handles.DrawLine(NewPoint_1, NewPoint_2);
         }
 
-        if (Input.GetMouseButtonDown(0))
-        {
-            m_MouseVector = Input.mousePosition;
-            //取得滑鼠點擊位置直角坐標系
-            m_TranMousePoint = m_MouseVector;
-            m_TranMousePoint.x = m_MouseVector.x - m_ScreenBaseVector.x - m_BasePoint_1.x;
-            m_TranMousePoint.y = m_MouseVector.y - m_ScreenBaseVector.y - m_BasePoint_1.y;
+        m_MouseVector = Input.mousePosition;
+        //取得滑鼠點擊位置直角坐標系
+        m_TranMousePoint = m_MouseVector;
+        m_TranMousePoint.x = m_MouseVector.x - m_ScreenBaseVector.x;
+        m_TranMousePoint.y = m_MouseVector.y - m_ScreenBaseVector.y;
 
-            float CosValue = CosThetaValue(BasePoint_2 - m_BasePoint_1, m_TranMousePoint - m_BasePoint_1);
-            Debug.Log(CosValue);
+        //算出滑鼠點擊座標和基準點兩點向量的Cosine值
+        float CosValue = CosThetaValue(NewBasePoint_2 - NewBasePoint_1, m_TranMousePoint - NewBasePoint_1);
+        //Debug.Log("CosVal: " + CosValue);
+        //將Cosine值轉換成角度
+        float Degree = CosToDregree(CosValue);
+        //Debug.Log("Degree: " + Degree);
+        //計算滑鼠點擊位置以及基準點1的距離
+        float Distance = VectorLength(m_TranMousePoint - NewBasePoint_1);
+        //Debug.Log("Distance: " + Distance);
+
+        //判斷滑鼠座標的角度是否在扇形張開的角度以及範圍內
+        if (Degree <= iSideDegree && Distance <= m_fLineLength)
+        {//表示在扇形張開的角度內
+            m_bIsInSector = true;
+        }
+        else
+        {
+            m_bIsInSector = false;
         }
     }
 
 
-
-    float GetRadian(float degreed)
+    float CosToDregree(float _CosValue)
     {
-        return degreed * Mathf.PI / 180;
+        return Mathf.Acos(_CosValue) * 180 / Mathf.PI;
     }
 
-    float Cos(float _degree)
+    float GetRadian(float _Degreed)
     {
-        return Mathf.Cos(GetRadian(_degree));
+        return _Degreed * Mathf.PI / 180;
     }
-    float Sin(float _degree)
+
+    float Cos(float _Degree)
     {
-        return Mathf.Sin(GetRadian(_degree));
+        return Mathf.Cos(GetRadian(_Degree));
+    }
+    float Sin(float _Degree)
+    {
+        return Mathf.Sin(GetRadian(_Degree));
     }
 
     float CosThetaValue(Vector3 _Vec1, Vector3 _Vec2)
